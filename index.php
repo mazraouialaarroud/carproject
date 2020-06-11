756b052ef12a801c29a5a875f82667f77d52ec1c
mazraouimoad <m.mazraoui1999@hotmail.com>
2020-06-11 17:54:19 +0100

    modified index.php

--- index.php
+++ index.php
@@ -192,9 +192,7 @@ foreach($results as $result)
 <!-- Scripts --> 
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script> 
-<script src="assets/js/interface.js"></script> 
-<!--Switcher-->
-<script src="assets/switcher/js/switcher.js"></script>
+<script src="assets/js/interface.js"></script>
 <!--bootstrap-slider-JS--> 
 <script src="assets/js/bootstrap-slider.min.js"></script> 
 <!--Slider-JS--> 
